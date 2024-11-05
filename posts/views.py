d37c6713c65b511e5fe45bbd1b522a52e2c9a6d5
Rogers-Wandera <rogerrisha@gmail.com>
2024-11-05 08:08:29 +0300

    django admin and rendering posts from a model

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpRequest
+from .models import Post
 
-# Create your views here.
+
 def Post_List(request: HttpRequest):
-    return render(request, "posts/post_list.html")
+    posts = Post.objects.all().order_by("-date")
+    return render(request, "posts/post_list.html", {"posts": posts})
