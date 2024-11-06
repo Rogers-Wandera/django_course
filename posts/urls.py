3cfe29fd4fba2fd27c13545b557a02e98832f75e
Rogers-Wandera <rogerrisha@gmail.com>
2024-11-06 07:12:07 +0300

    added new changes

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = "posts"
+
 urlpatterns = [
-    path('', views.Post_List),
+    path('', views.Post_List, name="list"),
+    path('<slug:slug>', views.Post_Page, name="page")
 ]
--- posts/views.py
+++ posts/views.py
@@ -6,3 +6,7 @@ from .models import Post
 def Post_List(request: HttpRequest):
     posts = Post.objects.all().order_by("-date")
     return render(request, "posts/post_list.html", {"posts": posts})
+
+def Post_Page(request: HttpRequest, slug: str):
+    post = Post.objects.get(slug=slug)
+    return render(request, "posts/post_page.html", {"post": post})
